2f5416fb10ecc625a0e8b5eb6248ff417b37d0a4
Samuel Ballan <sgb4622@gmail.com>
2015-10-22 17:26:39 -0400

    Initial Commit

---
+++ server/auth/google.oauth.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var router = require('express').Router();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+var User = require('../api/users/user.model');
+
+router.get('/', passport.authenticate('google', {
+	scope: 'email'
+}));
+
+router.get('/callback', passport.authenticate('google', {
+	successRedirect: '/stories',
+	failureRedirect: '/signup'
+}));
+
+var secureData = require('../../secure/keys.js') || null
+var twitterData = secureData.google;
+
+passport.use(new GoogleStrategy({
+	clientID: twitterData.clientID,
+	clientSecret: twitterData.clientSecret,
+	callbackURL: 'http://127.0.0.1:8080/auth/google/callback'
+}, function (token, refreshToken, profile, done) {
+	User.findOne({'google.id': profile.id }, function (err, user) {
+		if (err) done(err);
+		else if (user) done(null, user);
+		else {
+			var email = profile.emails[0].value;
+			User.create({
+				email: email,
+				photo: profile.photos[0].value,
+				name: profile.displayName,
+				google: {
+					id: profile.id,
+					name: profile.displayName,
+					email: email,
+					token: token
+				}
+			}, done);
+		}
+	});
+}));
+
+module.exports = router;
